484d5b2c1f3bc3ad1e0fdfcc6e983d57ea471d83
Lalit Mangal <78521741+shinigami1908@users.noreply.github.com>
2022-04-12 21:13:41 +0530

    Create ReplayButton.java
    created file

---
+++ ReplayButton.java
@@ -0,0 +1,16 @@
+private void jButton4ActionPerformed
+(java.awt.event.ActionEvent evt) {
+	jTextField1.setText("");
+	jTextField2.setText("");
+	jTextField3.setText("");
+	jTextField4.setText("");
+	jTextField5.setText("");
+	jTextField6.setText("");
+	jTextField7.setText("");
+	jTextField8.setText("");
+	jTextField9.setText("");
+	jTextField10.setEditable(true);
+	jTextField11.setEditable(false);
+	jButton1.setEnabled(true);
+	jButton2.setEnabled(false);
+}
